7d22f12c2d0ecee7a8e9ac2079ff2c7adb4d6d87
Aldin Kiselica <aldin.kiselica.94@gmail.com>
2022-07-13 11:01:50 +0200

    Wip channels.common models boilerplate. Include lombok.

--- src/main/java/io/github/infobip_community/client/channels/common/FromTo.java
+++ src/main/java/io/github/infobip_community/client/channels/common/FromTo.java
@@ -2,8 +2,14 @@
 package io.github.infobip_community.client.channels.common;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 import org.hibernate.validator.constraints.Range;
 
+@Getter
+@EqualsAndHashCode
+@ToString
 public abstract class FromTo {
     @NotNull
     @Range(min = 0, max = 23)
@@ -25,48 +31,5 @@ public abstract class FromTo {
     public FromTo(Integer hour, Integer minute) {
         this.hour = hour;
         this.minute = minute;
-    }
-
-    public Integer getHour() {
-        return hour;
-    }
-
-    public Integer getMinute() {
-        return minute;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((hour == null) ? 0 : hour.hashCode());
-        result = prime * result + ((minute == null) ? 0 : minute.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (!(obj instanceof FromTo)) {
-            return false;
-        }
-        FromTo other = (FromTo) obj;
-        if (hour == null) {
-            if (other.hour != null) {
-                return false;
-            }
-        } else if (!hour.equals(other.hour)) {
-            return false;
-        }
-        if (minute == null) {
-            if (other.minute != null) {
-                return false;
-            }
-        } else if (!minute.equals(other.minute)) {
-            return false;
-        }
-        return true;
     }
 }
